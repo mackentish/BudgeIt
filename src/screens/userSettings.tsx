4e7dfc0a9aff817bd8d03d29734dd33bb6c1e9e3
Brandon Trzepacz Tish <brandon.tish@gmail.com>
2023-06-20 07:57:02 -0400

    add sign out button to user settings

--- src/screens/userSettings.tsx
+++ src/screens/userSettings.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { colors, font } from '../constants/globalStyle';
+import { AnimatedPressable } from '../components';
+import { UserContext } from '../state/context/UserProvider';
 
 export default function UserSettings() {
+  const { signOut } = useContext(UserContext);
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>User Settings</Text>
+      <AnimatedPressable style={styles.signOutBtn} onPress={signOut}>
+        <Text style={styles.signOutText}>Sign Out</Text>
+      </AnimatedPressable>
     </View>
   );
 }
@@ -14,6 +20,7 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'column',
+    gap: 16,
     alignItems: 'center',
     backgroundColor: colors.background,
     flex: 1,
@@ -22,6 +29,17 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 20,
     fontFamily: font.regular,
+    color: colors.white,
+    marginBottom: 10,
+  },
+  signOutBtn: {
+    padding: 10,
+    backgroundColor: colors.red,
+    borderRadius: 10,
+  },
+  signOutText: {
+    fontSize: 20,
+    fontFamily: font.semiBold,
     color: colors.white,
   },
 });
