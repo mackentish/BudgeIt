fe06d25a6d38781098e1948817075f920d8dc639
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-08-18 10:57:40 -0400

    remove unused storage key

--- src/constants/persistentStorage.ts
+++ src/constants/persistentStorage.ts
@@ -2,14 +2,5 @@ const userCredentialsKey = 'userCredentials';
 const deviceHasBiometricsKey = 'deviceHasBiometrics';
 const havePromptedForBiometricsKey = 'havePromptedForBiometrics';
 const haveBiometricsPermissionKey = 'haveBiometricsPermission';
-const persistNextLoginKey = 'persistNextLogin';
 
-// TODO: combine these into a single key that stores an object?
-
-export {
-  userCredentialsKey,
-  deviceHasBiometricsKey,
-  havePromptedForBiometricsKey,
-  haveBiometricsPermissionKey,
-  persistNextLoginKey,
-};
+export { userCredentialsKey, deviceHasBiometricsKey, havePromptedForBiometricsKey, haveBiometricsPermissionKey };
