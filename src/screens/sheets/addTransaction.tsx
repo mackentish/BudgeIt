f8bf7bd90a6b00b9b12aadc197eba022b6af9e62
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-10-17 14:57:17 -0400

    Fix date picker bug by removing from within KeyboardAvoidingView

--- src/screens/sheets/addTransaction.tsx
+++ src/screens/sheets/addTransaction.tsx
@@ -55,116 +55,119 @@ export default function AddTransaction({ navigation }: Props) {
   }
 
   return (
-    <KeyboardAvoidingView behavior="padding" style={styles.flex} keyboardVerticalOffset={bottom + 8}>
-      <View style={styles.container}>
-        <Pressable
-          onPress={() => {
-            Keyboard.dismiss();
-            close();
-          }}
-          style={styles.closeBtn}>
-          <Icon name="x" style={styles.close} />
-        </Pressable>
-        <ScrollView contentContainerStyle={styles.scroll}>
-          <TextInput
-            value={transactionTitle}
-            onChangeText={setTransactionTitle}
-            placeholder="Title of Transaction"
-            style={styles.title}
-            autoCorrect={false}
-            spellCheck={false}
-          />
-          <CurrencyInput value={transactionAmount} setValue={setTransactionAmount} style={styles.amount} />
-          <View style={styles.details}>
-            <Pressable onPress={() => setDatePickerVisibility(true)} style={[styles.input, styles.datePickerRow]}>
-              <Text style={styles.text}>{date.toLocaleDateString()}</Text>
-              <AnimatedChevron chevronUp={isDatePickerVisible} />
-            </Pressable>
-            <DateTimePickerModal
-              isVisible={isDatePickerVisible}
-              date={date}
-              mode="date"
-              display="inline"
-              onConfirm={newDate => {
-                setDate(newDate);
-                setDatePickerVisibility(false);
-              }}
-              onCancel={() => setDatePickerVisibility(false)}
-              buttonTextColorIOS={colors.temp.black}
-              accentColor={colors.temp.black}
-            />
-
-            <Dropdown
-              label="Inflow"
-              placeholder="Where's the money coming from?"
-              options={flowOptions}
-              value={inflow}
-              setValue={setInflow}
-              topOption={externalOption}
-            />
-
-            <Dropdown
-              label="Outflow"
-              placeholder="Where's the money going?"
-              options={flowOptions}
-              value={outflow}
-              setValue={setOutflow}
-              topOption={externalOption}
-            />
-
-            <AnimatedPressable style={styles.tagPressable} onPress={() => navigation.navigate('selectTags')}>
-              <View style={styles.tagContainer}>
-                <Text style={styles.text}>Select Tags</Text>
-                {transactionTags.length > 0 && (
-                  <Text style={styles.smallText}>
-                    {transactionTags.length <= 3
-                      ? transactionTags.join(', ')
-                      : transactionTags.slice(0, 3).join(', ') +
-                        (transactionTags.length > 3 && ` +${transactionTags.length - 3} more`)}
-                  </Text>
-                )}
-              </View>
-              <Icon name="chevron-right" style={styles.chevron} />
-            </AnimatedPressable>
-
+    <View style={styles.flex}>
+      <KeyboardAvoidingView behavior="padding" style={styles.flex} keyboardVerticalOffset={bottom + 18}>
+        <View style={styles.container}>
+          <Pressable
+            onPress={() => {
+              Keyboard.dismiss();
+              close();
+            }}
+            style={styles.closeBtn}>
+            <Icon name="x" style={styles.close} />
+          </Pressable>
+          <ScrollView contentContainerStyle={styles.scroll}>
             <TextInput
-              value={note}
-              onChangeText={setNote}
-              placeholder="Add a note"
-              style={styles.input}
+              value={transactionTitle}
+              onChangeText={setTransactionTitle}
+              placeholder="Title of Transaction"
+              style={styles.title}
               autoCorrect={false}
               spellCheck={false}
             />
-          </View>
-          <View style={styles.flex} />
-          <Button
-            label="Add Transaction"
-            onPress={() => {
-              createTransaction.mutate(
-                {
-                  name: transactionTitle,
-                  amount: parseFloat(transactionAmount.replace(/[^0-9.-]+/g, '')),
-                  date,
-                  // it's okay to force unwrap here because we know that inflow and outflow are defined
-                  inflow: inflow!.value,
-                  outflow: outflow!.value,
-                  tags: transactionTags,
-                  note,
-                },
-                {
-                  onSuccess: () => {
-                    fetchPockets.refetch();
-                    close();
-                    resetForm();
+            <CurrencyInput value={transactionAmount} setValue={setTransactionAmount} style={styles.amount} />
+            <View style={styles.details}>
+              <Pressable onPress={() => setDatePickerVisibility(true)} style={[styles.input, styles.datePickerRow]}>
+                <Text style={styles.text}>{date.toLocaleDateString()}</Text>
+                <AnimatedChevron chevronUp={isDatePickerVisible} />
+              </Pressable>
+
+              <Dropdown
+                label="Inflow"
+                placeholder="Where's the money coming from?"
+                options={flowOptions}
+                value={inflow}
+                setValue={setInflow}
+                topOption={externalOption}
+              />
+
+              <Dropdown
+                label="Outflow"
+                placeholder="Where's the money going?"
+                options={flowOptions}
+                value={outflow}
+                setValue={setOutflow}
+                topOption={externalOption}
+              />
+
+              <AnimatedPressable style={styles.tagPressable} onPress={() => navigation.navigate('selectTags')}>
+                <View style={styles.tagContainer}>
+                  <Text style={styles.text}>Select Tags</Text>
+                  {transactionTags.length > 0 && (
+                    <Text style={styles.smallText}>
+                      {transactionTags.length <= 3
+                        ? transactionTags.join(', ')
+                        : transactionTags.slice(0, 3).join(', ') +
+                          (transactionTags.length > 3 && ` +${transactionTags.length - 3} more`)}
+                    </Text>
+                  )}
+                </View>
+                <Icon name="chevron-right" style={styles.chevron} />
+              </AnimatedPressable>
+
+              <TextInput
+                value={note}
+                onChangeText={setNote}
+                placeholder="Add a note"
+                style={styles.input}
+                autoCorrect={false}
+                spellCheck={false}
+              />
+            </View>
+            <View style={styles.flex} />
+            <Button
+              label="Add Transaction"
+              onPress={() => {
+                createTransaction.mutate(
+                  {
+                    name: transactionTitle,
+                    amount: parseFloat(transactionAmount.replace(/[^0-9.-]+/g, '')),
+                    date,
+                    // it's okay to force unwrap here because we know that inflow and outflow are defined
+                    inflow: inflow!.value,
+                    outflow: outflow!.value,
+                    tags: transactionTags,
+                    note,
                   },
-                },
-              );
-            }}
-            disabled={!isValid}
-          />
-        </ScrollView>
-      </View>
-    </KeyboardAvoidingView>
+                  {
+                    onSuccess: () => {
+                      fetchPockets.refetch();
+                      close();
+                      resetForm();
+                    },
+                  },
+                );
+              }}
+              disabled={!isValid}
+            />
+          </ScrollView>
+        </View>
+      </KeyboardAvoidingView>
+      {/** Put date picker outside of KeyboardAvoidingView so the app doesn't freeze */}
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        date={date}
+        mode="date"
+        display="inline"
+        onConfirm={newDate => {
+          setDate(newDate);
+          setDatePickerVisibility(false);
+        }}
+        onCancel={() => setDatePickerVisibility(false)}
+        buttonTextColorIOS={colors.temp.black}
+        accentColor={colors.temp.black}
+      />
+    </View>
   );
 }
 
